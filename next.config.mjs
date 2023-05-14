9a4eca2db50fc3d664a9686cd4c3ad18543ae137
masapasa <apyakurel@gmail.com>
2023-05-14 15:17:38 +0200

    cors

--- next.config.mjs
+++ next.config.mjs
@@ -21,13 +21,30 @@ const nextConfig = {
       "lh3.googleusercontent.com",
       "avatars.githubusercontent.com",
       // Add your own bucket urls here:
-      "t3-images-bucket.s3.sa-east-1.amazonaws.com",
-      "t3-images-bucket.s3.amazonaws.com",
-      "t3-avatars-bucket.s3.sa-east-1.amazonaws.com",
-      "t3-avatars-bucket.s3.amazonaws.com",
-      "t3-tag-images-bucket.s3.sa-east-1.amazonaws.com",
+      "t3-image-bucket.s3.us-east-1.amazonaws.com",
+      "t3-image-bucket.s3.amazonaws.com",
+      "t3-avatar-bucket.s3.us-east-1.amazonaws.com",
+      "t3-avatar-bucket.s3.amazonaws.com",
+      "t3-tag-image-bucket.s3.us-east-1.amazonaws.com",
+      "t3-tag-image-bucket.s3.amazonaws.com",
+      "t3-tag-image-bucket.s3.us-east-1.amazonaws.com",
+      "t3-tag-image-bucket.s3.amazonaws.com",
     ],
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
